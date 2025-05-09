893004c7112bb0d20526768d971cea5275eceb10
Arvs013 <ortegaharvey013@gmail.com>
2025-05-10 03:55:58 +0800

    UserProfile

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -33,7 +33,8 @@ const Register = () => {
         contactNumber: formData.contactNumber,
         address: formData.address
       };
-      localStorage.setItem("userProfile", JSON.stringify(userData));
+      localStorage.setItem("userData", JSON.stringify(userData));
+
 
       alert("Registration successful");
       window.location.href = "/landingpage"; // Redirect to login or another page after registration
